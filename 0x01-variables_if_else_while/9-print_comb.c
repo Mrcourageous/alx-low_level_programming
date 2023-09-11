cd065ee025e687fd9026d9342ac53193699119c5
Mrcourageous <couragec625@gmail.com>
2023-09-11 08:59:32 -0700

    hello world first day on c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the code
+ * the code that print all posiible combination of a single digit
+ * Return: 0
+ */
+int main(void)
+{
+	int vas;
+
+	for  (vas = 48; vas <= 57; vas++)
+	{
+		putchar(vas);
+		if (vas < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
