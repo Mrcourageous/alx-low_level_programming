6ac7a735bd12b307646eabd3ba65a0584312d3d2
Mrcourageous <couragec625@gmail.com>
2023-09-11 07:51:58 -0700

    Modupe

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * the code
+ * the code that print numbers startingg from'0'
+ * Return: 0
+ */
+int main(void)
+{
+	int zazu;
+
+	for (zuzu = 48; zazu <= 57; zazu++)
+	{
+		putchar(zazu);
+	}
+	putchar('\n');
+	return (0);
+}
