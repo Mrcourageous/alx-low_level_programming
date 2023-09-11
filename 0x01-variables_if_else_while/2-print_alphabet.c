883096202dd6dfbaf94ed9785ecb6e5c24056545
Mrcourageous <couragec625@gmail.com>
2023-09-11 06:52:53 -0700

    Putchar using string

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The code for block
+ * the code to print alphabets in lowlowercase using putchar
+ * Return: Always 0 success
+ */
+tnt main(void)
+{
+	char modupe;
+
+	for (modupe = 'a'; modupe <= 'z'; modupe++)
+	{
+		putchar(modupe);
+	}
+	putchar('\n');
+	return (0);
+}
