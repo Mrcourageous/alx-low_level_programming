af5cf49ae81f0549eed63194ce040fdf034df878
Mrcourageous <couragec625@gmail.com>
2023-09-11 08:10:12 -0700

    print a new lie starting from 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the code
+ * the code that print numbers stating from 10 
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 48; number < 58; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
