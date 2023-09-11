e3dfacfef183fbe79112b8011827ef525f26904e
Mrcourageous <couragec625@gmail.com>
2023-09-11 07:18:47 -0700

    good day wit alx

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - the code for lowercase
+ * How to print lowercase and uppercase using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char modupe;
+	
+	for (modupe = 'a'; modupe <= 'z'; modupe++)
+	{
+		putchar(modupe);
+	}
+	for (modupe = 'A'; modupe <= 'Z'; modupe++)
+	{
+		putchar(modupe);
+	}
+	putchar('\n');
+	return (0);
+}
