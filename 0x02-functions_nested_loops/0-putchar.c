19d7e01a0c905331073cbac6150aa912011bb080
Mrcourageous <couragec625@gmail.com>
2023-09-12 10:40:19 -0700

    nested loop in c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Description: the code 
+ * main - A program that print putchar follwed by a new line
+ * Return: 0
+ */
+int min(void)
+
+{
+	putchar("_putchar\n");
+	return (0);
+}
