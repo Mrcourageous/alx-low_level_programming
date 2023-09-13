ce3d2fc98162dc91d9518ef12fafe34abab64733
Mrcourageous <couragec625@gmail.com>
2023-09-13 06:25:20 -0700

    Printing all alphabet from a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * main - print_alphabet
+ * Descriptino: Print the alphabets from a-z with anew line in lowercase
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
