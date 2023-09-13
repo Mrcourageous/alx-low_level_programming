8242cf05ddcabdbad354ca9deca6e73f5fb42f57
Mrcourageous <couragec625@gmail.com>
2023-09-13 08:35:40 -0700

    Alx c days

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,21 @@
-#include "main.h"
+nclude "main.h"
 /**
- * Print_alphabet_10x - the alphabet
+ * print_alphabet_10x - make alphabet x10 times
  * Return: Always 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_10(void)
 {
 	char c;
-	int i;
-
-	i = 0;
-
-	while (i < 10)
+	int i=0
+	
+	while (i <= 0)
 	{
-		c = 'a';
-		while (c <= 'z')
-		{
-			_putchar(c);
-			c++;
-		}
-		_putchar('\n');
-		i++;
+	for (c = 'a'; c <= 'z'; c++)
 	}
+	_putchar(c);
+	{
+	_putchar('\n');
+	i++;
+	}
+		
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - the code 
+ * @c: The code to check
+ * Return: Always 0
+ */
+int _islower(int c);
+{
+	if(c >= 'a' && c <= 'z')
+	{
+	return(i);
+	}
+	else
+	{
+	return(0);
+	}	}
+}
